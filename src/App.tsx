5441dad80733fd4e6e6c5186263a92c59ebf9291
sean <sean@gomicorp.com>
2022-06-22 17:25:20 +0900

    add dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import './App.css';
 
+import { Dropdown } from './components/dropdown/dropdown';
 import { DefaultRadio } from './components/radio/default-radio';
+
+const options = [
+  { value: 'green', label: 'Green' },
+  { value: 'blue', label: 'Blue' },
+  { value: 'red', label: 'Red' },
+  { value: 'orange', label: 'Orange' },
+  { value: 'yellow', label: 'Yellow' },
+];
 
 const App = () => {
   return (
     <div className=''>
       <section className='p-6 flex flex-col'>
         <DefaultRadio />
+        <br />
+        <Dropdown placeHolder='Select...' options={options} isMulti />
       </section>
     </div>
   );
---
+++ src/components/dropdown/dropdown.tsx
@@ -0,0 +1,114 @@
+import './dropdown.css';
+
+import { MouseEvent, ReactNode, useEffect, useState } from 'react';
+
+import { CloseIcon, DownArrowIcon } from './dropdown-icons';
+
+type Option = {
+  value: string;
+  label: string;
+};
+
+type DropdownProps = {
+  placeHolder: ReactNode;
+  options: Option[];
+  isMulti?: boolean;
+};
+
+export const Dropdown = ({ placeHolder, options, isMulti }: DropdownProps) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [selected, setSelected] = useState<Option | undefined>(undefined);
+  const [multiSelected, setMultiSelected] = useState<Option[] | undefined>(undefined);
+
+  useEffect(() => {
+    const handler = () => setShowMenu(false);
+    window.addEventListener('click', handler);
+    return () => {
+      window.removeEventListener('click', handler);
+    };
+  });
+
+  const onClickInput = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setShowMenu(!showMenu);
+  };
+
+  const removeOption = (option: Option) => {
+    if (!multiSelected) return [];
+    return multiSelected.filter((o) => o.value !== option.value);
+  };
+
+  const onClickItem = (option: Option) => {
+    setSelected(option);
+  };
+
+  const onClickMultiItem = (option: Option) => {
+    if (!isMulti) return;
+    if (multiSelected?.find((o) => o.value === option.value)) return;
+    const newOptions = multiSelected ? [...multiSelected, option] : [option];
+    setMultiSelected(newOptions);
+  };
+
+  const isSelected = (option: Option) => {
+    if (!selected) {
+      return false;
+    }
+    return selected.value === option.value;
+  };
+
+  const onTagRemove = (e: MouseEvent<HTMLDivElement>, option: Option) => {
+    e.stopPropagation();
+    if (!multiSelected) return;
+    setMultiSelected(removeOption(option));
+  };
+
+  const getDisplay = () => {
+    if (selected) {
+      return selected.label;
+    }
+    if (isMulti) {
+      if (!multiSelected) {
+        return placeHolder;
+      }
+      return (
+        <div className='dropdown-tags'>
+          {multiSelected.map((option) => (
+            <div key={option.value} className='dropdown-tag-item'>
+              {option.label}
+              <span onClick={(e) => onTagRemove(e, option)}>
+                <CloseIcon />
+              </span>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    return placeHolder;
+  };
+
+  return (
+    <div className='dropdown-container'>
+      <div className='dropdown-input' onClick={onClickInput}>
+        <div className='dropdown-selected-value'>{getDisplay()}</div>
+        <div className='dropdown-tools'>
+          <div className='dropdown-tool'>
+            <DownArrowIcon />
+          </div>
+        </div>
+      </div>
+      {showMenu && (
+        <div className='dropdown-menu'>
+          {options.map((option) => (
+            <div
+              key={option.value}
+              onClick={() => (isMulti ? onClickMultiItem(option) : onClickItem(option))}
+              className={`dropdown-item ${isSelected(option)}`}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
